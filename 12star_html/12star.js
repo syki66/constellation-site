7e6723f66d23cbdd6d20919296b44e3039d7916b
syki66 <syeonki66@gmail.com>
2019-06-17 21:18:40 +0900

    finish

--- 12star_html/12star.js
+++ 12star_html/12star.js
@@ -1,10 +1,38 @@
+document.getElementById(0).style.display="block";
+for(var i = 1; i<13; i++){
+  document.getElementById(i).style.display="none";
+}
+
+
 function star12_select(){
-  var s = document.getElementById("star12");
-  var s12 = ['1th_star.html','2th_star.html','3th_star.html','4th_star.html','5th_star.html','6th_star.html','7th_star.html','8th_star.html','9th_star.html','10th_star.html','11th_star.html','12th_star.html','all_star.html']
-  for(var i = 0; i<s12.length; i++){
-    if (s.options[s.selectedIndex].value == i){
-      document.location.href = s12[i-1];
+  var mth = document.getElementById("month_btn").value*1;
+  var day = document.getElementById("day_btn").value*1;
+  var cal = [31,28,31,30,31,30,31,31,30,31,30,31];
+  var star_cal = [30, 29, 31, 30, 32, 32, 31, 31, 30, 31, 29, 1000];
+  var result = 0;
+  var star_result = 21;
+
+  for(var i = 0; i<mth-1; i++){
+    result += cal[i];
+  }
+  result += day;
+
+  for(var i=0; i<12; i++){
+    star_result += star_cal[i];
+    if(result < star_result & star_result <1000 & result > 20){
+      for(var j = 0; j<13; j++){
+        document.getElementById(j).style.display="none";
+      }
+      document.getElementById(i+1).style.display="block";
+      break;
+    }
+    else{
+      for(var j = 0; j<13; j++){
+        document.getElementById(j).style.display="none";
+      }
+      document.getElementById(12).style.display="block";
     }
   }
+
 
 }
--- test/star.js
+++
@@ -1,4 +0,0 @@
-/*var month;
-
-month = prompt("태어난 월을 입력하세요 (숫자만입력)");
-alert(month);*/
